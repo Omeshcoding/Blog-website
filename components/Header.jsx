f944f36396d4a1320b78a85f7102f0215785bf70
Umesh Sharma <76892626+Omeshcoding@users.noreply.github.com>
2023-02-21 20:57:20 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Graph CMS
+             Omesha-Blog
             </span>
           </Link>
         </div>
