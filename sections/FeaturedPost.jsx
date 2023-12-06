28f58f20d7975d92550f842c248517af0daff6c1
umesh <umesharma.us11@gmail.com>
2023-12-06 14:41:25 +0530

    Style fixes

--- sections/FeaturedPost.jsx
+++ sections/FeaturedPost.jsx
@@ -33,10 +33,10 @@ const FeaturedPost = () => {
     });
   }, []);
   const customLeftArrow = (
-    <div className="absolute arrow-btn left-0 text-center cursor-pointer bg-pink-600 rounded-full py-3">
+    <div className="absolute arrow-btn left-0 text-center cursor-pointer gradient-style px-3 rounded-full">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 text-white w-full"
+        className="h-6 w-6  text-white     "
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
@@ -51,10 +51,10 @@ const FeaturedPost = () => {
     </div>
   );
   const customRightArrow = (
-    <div className="absolute arrow-btn right-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full ml-20">
+    <div className="absolute arrow-btn right-0 text-center py-3 cursor-pointer gradient-style rounded-full ml-20">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 text-white w-full"
+        className="h-6 w-6  text-white lg:w-full"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
